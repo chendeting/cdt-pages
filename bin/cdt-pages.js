374c07d0f6e1a816289fd19657ba654449c8c4f8
mac <>
2021-01-24 14:30:29 +0800

    添加文件，编写公共gulp编译文件，包装gulp cli

--- bin/cdt-pages.js
+++ bin/cdt-pages.js
@@ -1,3 +1,8 @@
-#!/user/bin/env node
+#!/usr/bin/env node
+process.argv.push('--cwd')
+process.argv.push(process.cwd())
+process.argv.push('--gulpfile')
+process.argv.push(require.resolve('../lib')) // require去寻找模块，resolve去对应的找路径
+require('gulp/bin/gulp')
 
-console.log('cdt  ==== cdt-pages')
+// console.log(process.argv)
